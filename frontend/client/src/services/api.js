d7c5384bedfdd51cfd03be2d222df8c861cc0765
Kaushika <23cseai6kaushika@gmail.com>
2026-03-15 10:27:32 +0530

    Fix API URL and routes

--- frontend/client/src/services/api.js
+++ frontend/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://mern-portfolio-yx0m.onrender.com/api' // Your LIVE backend URL
 });
 
 export default API;
